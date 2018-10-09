198504bbfb49d7e7aff184a556f40dbf6698f1fc
Lauren <davislau2001@gmail.com>
2018-10-09 12:20:36 -0600

    Added model group and renamed files.

---
+++ FirstIOSProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSProject
+//
+//  Created by Davis, Lauren on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}

364fa58942634bb1f361a22ea4856e3d55154000
Lauren <davislau2001@gmail.com>
2018-10-09 12:30:23 -0600

    Created a random color functions

--- FirstIOSProject/Model/ColorTool.swift
+++ FirstIOSProject/Model/ColorTool.swift
@@ -11,5 +11,17 @@ import UIKit
 
 public class ColorTool
 {
-    
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        //let redPercent be of type CGFloat = CGFloat of... math
+        let bluePercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        let greenPercent : CGFloat = CGFloat ((arc4random() % 256) / 255)
+        
+        //constructor declaring variable parameters
+        return UIColor(red: redPercent, green: greenPercent, blue: bluePercent, alpha: CGFloat(100))
+        //alpha on color is opacity (set to 100%)
+        
+        
+    }
 }
